6651ca62daf47658b7ca20364b734268cd9640bd
Mohamed Basem <eng_m.basem@outlook.com>
2021-08-19 14:18:30 +0200

    ...

---
+++ StrokeForEgypt.API/Services/EntityLocalizationService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace StrokeForEgypt.API.Services
+{
+    public class EntityLocalizationService
+    {
+        private readonly IStringLocalizer localizer;
+        public EntityLocalizationService(IStringLocalizerFactory factory)
+        {
+            AssemblyName assemblyName = new(typeof(EntityResources).GetTypeInfo().Assembly.FullName);
+            localizer = factory.Create(nameof(EntityResources), assemblyName.Name);
+        }
+
+        public string Get(string key)
+        {
+            return localizer[key];
+        }
+    }
+}
--- StrokeForEgypt.API/Startup.cs
+++ StrokeForEgypt.API/Startup.cs
@@ -1,9 +1,24 @@
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Linq;
+using StrokeForEgypt.API.Services;
+using StrokeForEgypt.Common;
+using StrokeForEgypt.DAL;
+using StrokeForEgypt.Repository;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
 
 namespace StrokeForEgypt.API
 {
@@ -20,11 +35,91 @@ namespace StrokeForEgypt.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllersWithViews()
+                    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+            services.AddDbContext<BaseDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("dbConnection")));
+
+            services.AddScoped<UnitOfWork>();
+
+            services.AddAutoMapper(c => c.AddProfile<Mapping>(), typeof(Startup));
+
+            AppMainData.DomainName = Configuration.GetValue<string>("DomainName");
+
+            #region Swagger
+
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "StrokeForEgypt.API", Version = "v1" });
+                c.SwaggerDoc("MainData", new OpenApiInfo { Title = "Main Data", Version = "v1" });
+
+                string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
+
+            #endregion
+
+            #region Cors
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .WithExposedHeaders("X-Status", "X-Pagination")
+                           .AllowAnyHeader();
+                });
+            });
+
+            #endregion
+
+            #region Versioning
+
+            services.AddApiVersioning(config =>
+            {
+                // Specify the default API Version
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                // If the client hasn't specified the API version in the request, use the default API version number 
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                // Advertise the API versions supported for the particular endpoint
+                config.ReportApiVersions = true;
+            });
+
+            #endregion
+
+            #region Localization
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                CultureInfo[] supportedCultures = new[]
+                {
+                    new CultureInfo("en"),
+                    new CultureInfo("ar")
+                };
+                options.DefaultRequestCulture = new RequestCulture(culture: "ar", uiCulture: "ar");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+
+            services.AddSingleton<EntityLocalizationService>();
+
+            #endregion
+
+            #region Notification
+
+            // Notification Service
+            JToken jAppSettings = JToken.Parse(
+                                 File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "appsettings.json")));
+            string googleCredential = jAppSettings["GoogleCredential"].ToString();
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromJson(googleCredential)
+            });
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,15 +128,23 @@ namespace StrokeForEgypt.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StrokeForEgypt.API v1"));
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/MainData/swagger.json", "Main Data API");
+
+            });
+
+            AppMainData.WebRootPath = env.ContentRootPath;
+
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
+            app.UseStaticFiles();
+            app.UseFileServer();
+            app.UseCors();
+            app.UseRequestLocalization(app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>().Value);
 
             app.UseEndpoints(endpoints =>
             {
